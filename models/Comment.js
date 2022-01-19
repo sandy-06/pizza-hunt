3079757be62183577fac4bf4f722d159eb2f406c
Sandra Sanders <sandy-sanders@msn.com>
2022-01-19 12:48:49 -0700

    18.3done

--- models/Comment.js
+++ models/Comment.js
@@ -3,11 +3,11 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
   {
-    //set custom id to avoid confusion with parent comment_id
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId() 
-      },
+      default: () => new Types.ObjectId()
+    },
     replyBody: {
       type: String
     },
@@ -29,26 +29,29 @@ const ReplySchema = new Schema(
 
 const CommentSchema = new Schema(
   {
-  writtenBy: {
-    type: String
+    writtenBy: {
+      type: String
+    },
+    commentBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  replies: [ReplySchema]
-},
-{
-  toJSON: {
-    virtuals: true,
-    getters: true
-  },
-  id: false
-}
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
 );
+
 CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
 });
